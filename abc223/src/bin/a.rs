22ce86b718c841b50edfaa36d0f60ee109014fa9
ebihara99999 <dev.biibiebi@gmail.com>
2021-10-23 05:07:59 +0900

    Submit the a problem.

--- abc223/src/bin/a.rs
+++ abc223/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        x: i32,
+    };
+
+    let answer: &str = if x % 100 == 0 && x > 0 {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", answer);
 }
