fd111c0d3a9bf202c39f09d5fb7c273c7160c124
ebihara99999 <dev.biibiebi@gmail.com>
2021-10-23 06:17:31 +0900

    Submit a.

--- abc222/src/bin/a.rs
+++ abc222/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        x: i32,
+    };
+
+    if x / 1000 > 0 {
+        println!("{}", &x);
+    } else if x / 100 > 0 {
+        println!("0{}", &x);
+    } else if x / 10 > 0 {
+        println!("00{}", &x);
+    } else {
+        println!("000{}", &x);
+    };
 }
