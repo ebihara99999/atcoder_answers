74b903986436e94a0faa25fdb4036e62475abe81
ebihara99999 <dev.biibiebi@gmail.com>
2021-10-24 21:43:00 +0900

    Use formatted string.

--- abc222/src/bin/a.rs
+++ abc222/src/bin/a.rs
@@ -6,13 +6,5 @@ fn main() {
         x: i32,
     };
 
-    if x / 1000 > 0 {
-        println!("{}", &x);
-    } else if x / 100 > 0 {
-        println!("0{}", &x);
-    } else if x / 10 > 0 {
-        println!("00{}", &x);
-    } else {
-        println!("000{}", &x);
-    };
+    println!("{: >04}", x);
 }
