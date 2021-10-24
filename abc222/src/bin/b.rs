08fb4c5de78013023fa8df6fb1d65c967dbf2148
ebihara99999 <dev.biibiebi@gmail.com>
2021-10-24 21:56:05 +0900

    Submit b.

--- abc222/src/bin/b.rs
+++ abc222/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::{input};
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        p: u8,
+        a: [u8; n]
+    }
+
+    
+    let mut counter = 0;
+    for i in &a {
+        if i < &p {
+            counter += 1;
+        }
+    }
+    println!("{}", counter);
 }
