1fc9b2ff990b210f2773891bb43905e0f2ce1fa0
ebihara99999 <dev.biibiebi@gmail.com>
2021-10-27 03:37:21 +0900

    Submit b.

--- abc221/src/bin/b.rs
+++ abc221/src/bin/b.rs
@@ -1,3 +1,27 @@
+use std::process;
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: String,
+        m: String
+    }
+
+    if n == m {
+        println!("Yes");
+        process::exit(0);
+    }
+
+    let mut n_vec = n.chars().collect::<Vec<char>>();
+    let m_vec = m.chars().collect::<Vec<char>>();
+
+    for i in 0..n.len()-1 as usize {
+        n_vec.swap(i, i+1);
+        if n_vec == m_vec {
+            println!("Yes");
+            process::exit(0);
+        }
+        n_vec.swap(i, i+1);
+    }
+    println!("No");
 }
